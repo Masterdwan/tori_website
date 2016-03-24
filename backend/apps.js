6b51cb630b01759cdac9173597348013e8f16c42
jrbella <jbella@icstars.com>
2016-03-24 15:03:20 +0000

    u

--- backend/apps.js
+++ backend/apps.js
@@ -1,4 +1,5 @@
 var express = require('express');
+ var database = require('./database.js');
 var app = express();
 var port = process.env.PORT || 1337;
 //Set up to render the html correctly from the html folder
@@ -11,9 +12,32 @@ app.get('/', function(req,res){
   return res.status(200).send("hello worlds");
 });
 
+app.get('/trainList', function(req,res){
+  console.log('app / trains requested');
+  return res.render('trainList.html');
+});
+
+app.get('/trainList2', function(req,res){
+  console.log('app / trains requested');
+  return res.render('trainList2.html');
+});
+
+// This is my URL endpoint which is /get trains
+
+app.get('/getTrains', function(req,res){
+  console.log('app / getTrains requested');
+ 
+database.executeQuery("SELECT * FROM trains", function(results) {
+      res.send(results);
+
+//this is bringing back JSON not rendering HTML
+  
+});
+});
+
 app.get('/helloworld', function(req,res){
   console.log('app helloworld/ requested');
-  return res.render("helloworld.html");
+  return res.rvender("trainList.html");
 });
 
 app.listen(port, function(){
